a6633b7d61341f4094c8aaf132457635ea1255be
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2024-04-25 16:17:04 +0200

    Caddy Config, fixed Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM golang:latest AS build
 WORKDIR /opt/app/
 COPY app/ .
-
 RUN cd api/src && ls -la && go build -o ../build/api main.go
-
-# Stage 2: Create a minimal image to run the application
-FROM scratch
-WORKDIR /opt/api
-COPY --from=build /opt/app/api/build/api api
-CMD ["./api"]
+RUN ls -la
+CMD ["/opt/app/api/build/api"]
--- app/api/src/main.go
+++ app/api/src/main.go
@@ -1,21 +1,28 @@
 package main;
-//import "src/test";
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
+
+
+
+
 func main() {
-    // Define the directory to serve static files from
-    publicDir := "../../public"
-    // Create a file server handler for the specified directory
+	argsWithProg := os.Args
+	fmt.Println(argsWithProg);
+
+    publicDir := "/opt/app/public"
+
     fileServer := http.FileServer(http.Dir(publicDir))
-    // Register the file server handler to the root route
     http.Handle("/", fileServer)
-    // Start the server on port 8000
     fmt.Println("Server is running on port 8000")
     err := http.ListenAndServe(":8000", nil)
     if err != nil {
-        fmt.Println("Error starting server:", err)
+        fmt.Println("Error starting File server:", err)
     }
+
+
+
 }
