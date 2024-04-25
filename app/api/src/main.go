2660250ba9e4264f394e96eeed4df4cf270e967b
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2024-04-25 13:29:18 +0200

    added basic Http Server

--- app/api/src/main.go
+++ app/api/src/main.go
@@ -1,6 +1,21 @@
 package main;
-import "src/test";
+//import "src/test";
+import (
+    "fmt"
+    "net/http"
+)
 
-func main(){
-	test.Test();
+func main() {
+    // Define the directory to serve static files from
+    publicDir := "../../public"
+    // Create a file server handler for the specified directory
+    fileServer := http.FileServer(http.Dir(publicDir))
+    // Register the file server handler to the root route
+    http.Handle("/", fileServer)
+    // Start the server on port 8000
+    fmt.Println("Server is running on port 8000")
+    err := http.ListenAndServe(":8000", nil)
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+    }
 }
