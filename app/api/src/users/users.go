7563458bc47525a4a026e48f842f7065c1130ada
Chris <sent.pork@gmail.com>
2024-05-31 19:48:41 +0200

    finished users ig

--- app/api/src/users/users.go
+++ app/api/src/users/users.go
@@ -12,7 +12,6 @@ type User struct {
 	Password string
 	City     string
 	Username string
-	Admin    bool
 }
 
 type UserLoginInfo struct {
@@ -44,16 +43,24 @@ func GetUserCityById(id int32) (string, error) {
 }
 
 func GetUserByEmail(email string) (User, error) {
-	// Using GetUserIdByEmail to get the user id
-	id, err := GetUserIdByEmail(email)
-
-	// If there is an error, return the error
+	var db *sql.DB = db_utils.DB
+	rows, err := db.Query("SELECT id from USER WHERE email=? LIMIT 1", email)
 	if err != nil {
-		return User{}, err
+		return 0, err
+	}
+	defer rows.Close()
+	var id int32
+	id = 0
+
+	for rows.Next() {
+		rows.Scan(&id)
 	}
 
-	// Get User with id
-	return GetUserByID(int64(id)), nil
+	if id == 0 {
+		return 0, ErrNoUser
+	} else {
+		return id, nil
+	}
 }
 
 func GetUserIdByEmail(email string) (int32, error) {
@@ -79,48 +86,50 @@ func GetUserIdByEmail(email string) (int32, error) {
 
 // get user by id (assuming function needed)
 func GetUserByID(userId int64) User {
-	// TODO: Implement function
-	return User{}
-}
+	var db *sql.DB = db_utils.DB
+    query := "SELECT id, email, password, city, username FROM USER WHERE id=? LIMIT 1"
+    row := db.QueryRow(query, userId)
 
-// send notification to a user
-func SendNotification(userId int64, message_json string) error {
-	// TODO: Implement function
-	return nil
-}
-
-// send notification to a city
-func SendNotificationToCity(city string, message_json string) error {
-	// TODO: Implement function
-	return nil
+    var user User
+    err := row.Scan(&user.UserId, &user.Email, &user.Password, &user.City, &user.Username)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            return user, ErrNoUser
+        }
+        return user, err
+    }
+    return user, nil
 }
 
 // delete a user
 func DeleteUser(userId int64) error {
-	// TODO: Implement function
-	return nil
+	var db *sql.DB = db_utils.DB
+    query := "DELETE FROM USER WHERE id=?"
+    _, err := db.Exec(query, userId)
+    if err != nil {
+        return err
+    }
+    return nil
 }
 
 // update user information
 func UpdateUser(newInfo User) error {
-	// TODO: Implement function
-	return nil
+	var db *sql.DB = db_utils.db
+	query := "UPDATE USER SET email=?, password=?, city=?, username=? WHERE id=?"
+    _, err := db.Exec(query, newInfo.Email, newInfo.Password, newInfo.City, newInfo.Username, newInfo.UserId)
+    if err != nil {
+        return err
+    }
+    return nil
 }
 
 // create a new user
 func CreateUser(user User) error {
-	// TODO: Implement function
-	return nil
-}
-
-// check if credentials are correct
-func CheckCorrectCredentials(username string, password string) bool {
-	// TODO: Implement function
-	return false
-}
-
-// get notifications for a user
-func GetNotifications(userId int64) []string {
-	// TODO: Implement function
-	return []string{}
+	var db *sql.DB = db_utils.DB
+    query := "INSERT INTO USER (email, password, city, username) VALUES (?, ?, ?, ?)"
+    _, err := db.Exec(query, user.Email, user.Password, user.City, user.Username)
+    if err != nil {
+        return err
+    }
+    return nil
 }
