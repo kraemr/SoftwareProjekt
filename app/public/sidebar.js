4184b8bc1b47d163c4cb0b5a72d72ab2f2aae3cf
Entey <entey.community@gmail.com>
2024-06-13 21:39:16 +0200

    add working dynamic sidebar / bottom bar

--- app/public/sidebar.js
+++ app/public/sidebar.js
@@ -1,24 +1,49 @@
+
 function toggleSettings() {
     const settingsContainer = document.querySelector('.settings-container');
     settingsContainer.style.display = (settingsContainer.style.display === 'block') ? 'none' : 'block';
-  }
-  
-  // Optional: Close the settings when clicking outside of it
-  document.addEventListener('click', function(event) {
+}
+
+// Optional: Close the settings when clicking outside of it
+document.addEventListener('click', function (event) {
     const userContainer = document.querySelector('.user-container');
     const settingsContainer = document.querySelector('.settings-container');
     if (!userContainer.contains(event.target) && !settingsContainer.contains(event.target)) {
-      settingsContainer.style.display = 'none';
+        settingsContainer.style.display = 'none';
     }
-  });
-  
+});
+window.addEventListener('resize', function () {
+    const sidepanel = document.getElementById("sidepanel-toggle");
+    const button = document.getElementById("toggleButton");
+    // This code will run whenever the window is resized
+    if (window.innerWidth <= 768) {
+        // Change the sidebar to the small screen layout
+        console.log('The window is small');
+        sidepanel.style.height = "0";
+        sidepanel.style.top = "auto";
+        sidepanel.style.bottom = "0";
+        sidepanel.style.width = "100%";
+        button.style.bottom = "2px";
+        button.style.left = "50%";
+    } else {
+        // Change the sidebar to the large screen layout
+        sidepanel.style.height = "100%";
+        sidepanel.style.top = "0";
+        sidepanel.style.bottom = "auto";
+        sidepanel.style.width = "25%";
+        button.style.bottom = "50%";
+        button.style.left = "2px";
+        console.log('The window is large');
+        if (sidepanel.style.width === "25%") {
+            button.style.left = "20%";
+        }
+    }
+});
 // Toggle left sidebar
 function toggleNav() {
-    const sidepanel = document.getElementById("mySidepanel");
+    const sidepanel = document.getElementById("sidepanel-toggle");
     const button = document.getElementById("toggleButton");
     if (window.innerWidth <= 768) { // Check if the screen size is small
-        button.style.left = "50%";
-        sidepanel.style.width = "100%";
         if (sidepanel.style.height === "40%") {
             sidepanel.style.height = "0";
             button.style.bottom = "2px";
@@ -27,7 +52,6 @@ function toggleNav() {
             button.style.bottom = "41%";
         }
     } else {
-        button.style.left = "2px";
         if (sidepanel.style.width === "25%") {
             sidepanel.style.width = "0";
             button.style.left = "2px";
