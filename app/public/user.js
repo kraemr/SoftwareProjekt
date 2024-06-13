909ca9bdf9d6f808718f00956b33993e6d61e12b
Entey <entey.community@gmail.com>
2024-06-13 20:48:08 +0200

    add template for user image

---
+++ app/public/user.js
@@ -0,0 +1,26 @@
+var userID;
+var username;
+var userImage;
+function getUserByEmail(email) {
+    fetch(document.location.origin + "/api/users?email=" + email, {
+        method: "GET",
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            userID = data[0].id;
+        })
+        .catch((error) => {
+            console.error("There was a problem with the request:", error);
+            alert("Failed to load markers. Please try again.");
+        });
+}
+function setUserSettings() {
+    const settingsContainer = document.querySelector('.user-icon-src');
+    settingsContainer.style.backgroundImage = "url('/images/user-icon.png')";
+}
