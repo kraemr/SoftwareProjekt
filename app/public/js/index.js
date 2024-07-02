9ef6295503d79961407d302f8cc410d09b5e7445
Eichenauer, Etienne <entey.community@gmail.com>
2024-07-02 23:07:54 +0200

    add your profile link

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -10,9 +10,6 @@ function loadUserSettings() {
             <span class="user-settings-button w-100" onclick="OpenFavourites()">Favourites</span>
         </div>
         <div class="col-12">
-            <span class="user-settings-button w-100" onclick="OpenHistory()">History</span>
-        </div>
-        <div class="col-12">
             <span class="user-settings-button w-100" onclick="OpenLogout()">Sign out</span>
         </div>
     `;
@@ -27,5 +24,37 @@ function loadUserSettings() {
     `;
         }
     });
+}
+function OpenProfile() {
+    window.location.href = "your_Profile.html";
+}
+function OpenFavourites() {
+    hideSidebarContent();
+    var apiUrl = document.location.origin + '/api/faourites';
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById('sidebar-content').innerHTML = `
+        <div class="col-12">
+            <h4 class="mb-3">Favourites</h4>
+        </div>
+        ${data.map(favourite => `
+            <div class="col-12">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">${favourite.title}</h5>
+                        <p class="card-text">${favourite.info}</p>
+                        <p class="card-text">${favourite.city}</p>
+                        <p class="card-text">${favourite.type}</p>
+                        <p class="card-text">${favourite.Stars}</p>
+                        <p class="card-text">${favourite.recommended_count}</p>
+                    </div>
+                </div>
+            </div>
+        `).join('')}
+    `;
+        }
+        );
 }
 
